072606c57387f34b3d940bc92ae028a91e0f733f
mxn <56229381+mxnster@users.noreply.github.com>
2023-09-27 16:24:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    api_key: "CAP-", // get your key here https://capmonster.cloud/
+    api_key: "CAP-", // get your key here https://www.capsolver.com/
     retries: 5,
     check: true, // only check mode, put wallet addresses in file wallets.txt, eligible wallets will go into the file eligible.txt
     claim: true // claim mode, provide private keys in file wallets.txt
